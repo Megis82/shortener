5bf781c93dd1e8991c91f40e57a87b9426866005
Evgeny <megis82@mail.ru>
2023-09-17 13:49:22 +0300

    10

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,9 @@ package config
 
 import (
 	"flag"
+	"log"
+
+	env "github.com/caarlos0/env/v6"
 )
 
 type Config struct {
@@ -11,13 +14,33 @@ type Config struct {
 	BaseURL string
 }
 
+type configEnv struct {
+	NetAddress string `env:"SERVER_ADDRESS"`
+	BaseURL    string `env:"BASE_URL"`
+}
+
 var ServerConfig Config
-var RunningConfig Config
 
 func ParseConfig() {
 
 	flag.StringVar(&ServerConfig.NetAddress, "a", ":8080", "network address")
 	flag.StringVar(&ServerConfig.BaseURL, "b", "", "base URL")
 	flag.Parse()
+
+	var cfg configEnv
+
+	err := env.Parse(&cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if cfg.NetAddress != "" {
+		ServerConfig.NetAddress = cfg.NetAddress
+	}
+
+	if cfg.BaseURL != "" {
+		ServerConfig.BaseURL = cfg.BaseURL
+	}
+
 	//fmt.Println(ServerConfig.NetAddress, ServerConfig.BaseURL)
 }
